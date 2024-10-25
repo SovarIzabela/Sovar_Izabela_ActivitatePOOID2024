37a50f18ecd4afcf98b43049e282e79225e4d19d
sovarizabela22@stud.ase.ro <sovarizabela22@stud.ase.ro>
2024-10-25 22:22:14 +0300

    commit0

--- p3/P3.cpp
+++ p3/P3.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<string>
 using namespace std;
@@ -8,28 +9,53 @@ class Preparat
 private:
 	string denumirePreparat;
 	char tipMancare; // F pentru fel principal , M pentru mic dejun , A pentru aperitiv, D pentru desert
-	int numarIngrediente;
 	float pretPreparat;
 	bool contineAlergeni;
+	const int codPreparat;
+	static string OriginePreparat;
+    char* numeRestaurant;
+	int numarIngrediente;
+	string* listaIngrediente;
+	float* pretIngrediente;
+	 
+
 public:
-	Preparat(string denumirePreparat, char tipMancare, int numarIngrediente, float pretPreparat, bool contineAlergeni)
+	Preparat(string denumirePreparat, char tipMancare, float pretPreparat, bool contineAlergeni, int codPreparat, const char*numeRestaurant, int numarIngrediente, string* listaIngrediente, float*pretIngrediente):codPreparat(codPreparat)
 	{
 		this->denumirePreparat = denumirePreparat;
 		this->tipMancare = tipMancare;
-		this->numarIngrediente = numarIngrediente;
 		this->pretPreparat = pretPreparat;
 		this->contineAlergeni = contineAlergeni;
+		this->numeRestaurant = new char[strlen(numeRestaurant) + 1];
+		strcpy(this->numeRestaurant, numeRestaurant);
+		this->numarIngrediente = numarIngrediente;
+		this->listaIngrediente = new string[this->numarIngrediente];
+		this->pretIngrediente = new float[this->numarIngrediente];
+
+		for (int i = 0; i < this->numarIngrediente; i++)
+		{
+
+			this->listaIngrediente[i] = listaIngrediente[i];
+			this->pretIngrediente[i] = pretIngrediente[i];
+		}
+
+
 	}
 
-	Preparat()
+	Preparat():codPreparat(0)
 	{
 		this->denumirePreparat = "Anonim";
 		this->tipMancare = 'u';
-		this->numarIngrediente = 0;
 		this->pretPreparat = 0;
 		this->contineAlergeni = 0;
-	}
+		this->numeRestaurant = new char[strlen("necunoscut") + 1];
+		strcpy(this->numeRestaurant, "necunoscut");
+		this->numarIngrediente = 0;
+		this->listaIngrediente = NULL;
+		this->pretIngrediente = NULL;
 
+	}
+	//getteri
 	string getDenumirePreparat()
 	{
 		return this->denumirePreparat;
@@ -40,10 +66,7 @@ public:
 		return this->tipMancare;
 	}
 
-	int getNumarIngrediente()
-	{
-		return this->numarIngrediente;
-	}
+	
 
 	float getPretPreparat()
 	{
@@ -55,6 +78,31 @@ public:
 		return this->contineAlergeni;
 	}
 
+	int getCodPreparat()
+	{
+		return this->codPreparat;
+	}
+
+	char* getNumeRestaurant()
+	{
+		return this->numeRestaurant;
+	}
+
+	int getNumarIngrediente()
+	{
+		return this->numarIngrediente;
+	}
+
+	string* getListaIngrediente()
+	{
+		return this->listaIngrediente;
+	}
+	float* getPretIngrediente()
+	{
+		return this->pretIngrediente;
+	}
+
+	//seteri
 	void setDenumirePreparat(string DenumireNouaPreparat)
 	{
 		this->denumirePreparat = DenumireNouaPreparat;
@@ -65,10 +113,7 @@ public:
 		this->tipMancare = TipNouMancare;
 	}
 
-	void setNumarIngrediente(int NumarNouIngrediente)
-	{
-		this->numarIngrediente = NumarNouIngrediente;
-	}
+	
 
 	void setPretpreparat(float PretNouPreparat)
 	{
@@ -84,9 +129,17 @@ public:
 	{
 		cout << "Denumirea Preparatului este: " << this->denumirePreparat << endl;
 		cout << "Tipul de mancare este : " << this->tipMancare << endl;
-		cout << "Numarul de Ingrediente este : " << this->numarIngrediente << endl;
 		cout << "Pretul preparatului este : " << this->pretPreparat << endl;
 		cout << "Contine Alergeni ? " << (this->contineAlergeni? "DA":"NU") << endl;
+		cout << "Cod preparat: " << this->codPreparat << endl;
+		cout << "Numele restaurantului este : " << this->numeRestaurant << endl;
+		cout << "Numarul de ingrediente este : " << this->numarIngrediente << endl;
+		cout << "Lista de ingrediente si pretul acestora: " << endl;
+		for (int i = 0; i < this->numarIngrediente; i++)
+		{
+			cout << "Ingredient : " << this->listaIngrediente[i] << endl;
+			cout << "Pret Ingredient: " << this->pretIngrediente[i] << endl;
+		}
 		cout << endl << endl;
 	}
 
@@ -94,20 +147,43 @@ public:
 
 };
 
+string Preparat::OriginePreparat = "Romania";
+
 void main()
 {
-	Preparat m1("Omleta", 'M', 5, 25,0);
-	Preparat m2("salata Cesar", 'A', 7, 35,1);
+	string listaingrediente1[] = { "oua", "ulei", "sare" };
+	float pretIngredient1[] = { 5.0, 1.0, 0.10 };
+	Preparat m1("Omleta", 'M', 25,0,123, "UrbanHouse", 3,listaingrediente1,pretIngredient1 );
+
+
+	string listaingrediente2[] = { "rosii", "castraveti", "pui", "branza", "ceapa", "dressing"};
+	float pretIngredient2[] = { 1.0, 1.0, 5.10, 2.0, 0.5, 2 };
+	Preparat m2("salata Cesar", 'A', 35,1, 124,"Mahala",6, listaingrediente2, pretIngredient2);
+	cout << "denumire preparat " << m2.getDenumirePreparat() << endl;
+	cout << "Tip mancare: " << m2.getTipMancare() << endl;
+	cout << "Pretul Preparatului: " << m2.getPretPreparat() << endl;
+	cout << "Contine alergeni? " << m2.getContineAlergeni() << endl;
+	cout << "Codul preparatului este: " << m2.getCodPreparat() << endl;
+	cout << "Numele restaurantului este : " << m2.getNumeRestaurant() << endl;
+	cout << "Numarul de ingrediente este : " << m2.getNumarIngrediente() << endl;
+	cout << "Lista de ingrediente si pretul acestora: " << endl;
+	for (int i = 0; i < m2.getNumarIngrediente(); i++)
+	{
+		cout<<"Ingredient : " << m2.getListaIngrediente()[i] << endl;
+		cout<<"Pret Ingredient: " << m2.getPretIngrediente()[i] << endl;
+	}
+
+	cout << endl << endl;
+
 	Preparat m3;
 
 	m1.afisare();
-	m2.afisare();
+	
 	m3.afisare();
 
 	m1.setPretpreparat(30);
 	m1.setContineAlergeni(1);
 	m1.setDenumirePreparat("Omleta cu cascaval");
-	m1.setNumarIngrediente(6);
 	m1.setTipMancare('A');
 	cout << "......Valoare modificata m1..............." << endl;
 	m1.afisare();
