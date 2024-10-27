f6c30e1b7d6a893ce4003ac25d9cd6256f2fe0ce
sovarizabela22@stud.ase.ro <sovarizabela22@stud.ase.ro>
2024-10-27 15:55:05 +0200

    commit

--- p1.cpp
+++ p1.cpp
@@ -168,7 +168,7 @@ Hotel(string denumireHotel, string adresaHotel, int numarAngajati, int numarCame
 	this->areMicDejunInclus = areMicDejunInclus;
 	this->pretCameraPeZi = 0;
 	//alocare memorie la char*
-	this->numeManagerHotel = new char[strlen("anonim") + 1];
+	this->numeManagerHotel = new char[strlen("Anonim") + 1];
 	//initializare la char*
 	strcpy(this->numeManagerHotel, "Anonim");
 	this->numarServiciiDisponibile = 0;
@@ -263,6 +263,162 @@ void setPretCameraZi(float PretNouCameraZi)
 {
 	this->pretCameraPeZi = PretNouCameraZi;
 }
+//setter pentru char*
+
+void setNumeManagerHotel(const char* numeManagerHotelNou)
+{
+	//1.dezalocam valoarea veche (apelam instr de dezalocare mem din destructor
+	if (this->numeManagerHotel != NULL)
+	{
+		delete[] this->numeManagerHotel;
+	}
+
+	//2.realocam spatiu pentru noua valoare
+	this->numeManagerHotel = new char[strlen(numeManagerHotelNou) + 1];
+	//3.reinitializam cu noua valoare
+	strcpy(this->numeManagerHotel, numeManagerHotelNou);
+
+}
+// setter pt vectori si numarator
+void setServicii(int numarServiciiDisponibileNou, float* preturiServiciiNoi, string*numeServiciiNoi)
+{
+	//stergem vechea valoare
+	if (this->preturiServicii != NULL)
+	{
+		delete[] this->preturiServicii;
+	}
+
+	if (this->numeServicii != NULL)
+
+	{
+		delete[] this->numeServicii;
+	}
+	//realocam numaratorul cu valoarea noua
+	this->numarServiciiDisponibile = numarServiciiDisponibileNou;
+	//realocam memorie pentru vectori
+	this->preturiServicii = new float[this->numarServiciiDisponibile];
+	this->numeServicii = new string[this->numarServiciiDisponibile];
+	//reinitializam cu noua valoare
+
+	for (int i = 0; i < this->numarServiciiDisponibile; i++)
+	{
+		this->preturiServicii[i] = preturiServiciiNoi[i];
+		this->numeServicii[i] = numeServiciiNoi[i];
+	}
+
+}
+
+
+//destructor
+
+~Hotel()
+{
+	//dezalocare de memorie+stergerea atributelor
+	if (this->numeManagerHotel != NULL)
+	{
+		delete[] this->numeManagerHotel;
+	}
+	
+	if (this->preturiServicii != NULL)
+	{
+		delete[] this->preturiServicii;
+	}
+
+	if (this->numeServicii != NULL)
+
+	{
+		delete[] this->numeServicii;
+	}
+	cout << "AICI S-A APELAT DESTRUCTORUL" << endl;
+} 
+
+  //constructorul de copiere
+Hotel(Hotel& obiectExistent):anDeschidereHotel(obiectExistent.anDeschidereHotel)
+{   
+
+	this->denumireHotel = obiectExistent.denumireHotel;
+	this->adresaHotel = obiectExistent.adresaHotel;
+	this->numarAngajati = obiectExistent.numarAngajati;
+	this->numarCamere = obiectExistent.numarCamere;
+	this->areMicDejunInclus = obiectExistent.areMicDejunInclus;
+	this->pretCameraPeZi = obiectExistent.pretCameraPeZi;
+
+	this->numeManagerHotel = new char[strlen(obiectExistent.numeManagerHotel) + 1];
+	
+	strcpy(this->numeManagerHotel, obiectExistent.numeManagerHotel);
+
+	this->numarServiciiDisponibile = obiectExistent.numarServiciiDisponibile;
+
+	this->preturiServicii = new float[obiectExistent.numarServiciiDisponibile];
+	this->numeServicii = new string[obiectExistent.numarServiciiDisponibile];
+
+	
+	for (int i = 0; i < this->numarServiciiDisponibile; i++)
+	{
+		this->preturiServicii[i] = obiectExistent.preturiServicii[i];
+		this->numeServicii[i] = obiectExistent.numeServicii[i];
+
+	}
+
+
+ }
+
+	
+	//operatorul = (ia 2 obiecte si pe unul il modifica pe baza celuilalt ex: h1 obiect macheta, h4 obiect ce va fi modificat pe baza h1)
+
+	Hotel&operator=(Hotel& obiectMacheta)
+	{
+		//1.destructor
+		if (this->numeManagerHotel != NULL)
+		{
+			delete[] this->numeManagerHotel;
+		}
+
+		if (this->preturiServicii != NULL)
+		{
+			delete[] this->preturiServicii;
+		}
+
+		if (this->numeServicii != NULL)
+
+		{
+			delete[] this->numeServicii;
+		}
+
+		//2.constructorul de copiere
+
+		this->denumireHotel = obiectMacheta.denumireHotel;
+		this->adresaHotel = obiectMacheta.adresaHotel;
+		this->numarAngajati = obiectMacheta.numarAngajati;
+		this->numarCamere = obiectMacheta.numarCamere;
+		this->areMicDejunInclus = obiectMacheta.areMicDejunInclus;
+		this->pretCameraPeZi = obiectMacheta.pretCameraPeZi;
+
+		this->numeManagerHotel = new char[strlen(obiectMacheta.numeManagerHotel) + 1];
+
+		strcpy(this->numeManagerHotel, obiectMacheta.numeManagerHotel);
+
+		this->numarServiciiDisponibile = obiectMacheta.numarServiciiDisponibile;
+
+		this->preturiServicii = new float[obiectMacheta.numarServiciiDisponibile];
+		this->numeServicii = new string[obiectMacheta.numarServiciiDisponibile];
+
+
+		for (int i = 0; i < this->numarServiciiDisponibile; i++)
+		{
+			this->preturiServicii[i] = obiectMacheta.preturiServicii[i];
+			this->numeServicii[i] = obiectMacheta.numeServicii[i];
+
+		}
+
+		//3. dam return
+
+		return*this;
+
+
+
+	}
+
 };
 
 int Hotel::clasaCAENHotel = 55;
@@ -404,8 +560,21 @@ void main() {
 	cout << "Anul deschiderii hotelului eate:" << h1.getAnDeschidereHotel() << endl;
 	cout << endl << endl;
 
+
+	cout << "//.......Apelare setteri........//" << endl<<endl;
+
 	h1.setNumarAngajati(55);
 	h1.setPretCameraZi(2500);
+	h1.setAdresaHotel("bd-ul Unirii, nr5");
+	h1.setAreMicDejunInclus(0);
+	h1.setNumeManagerHotel("Ionescu Maria");
+
+	float preturi2[] = { 450.20, 100.0, 200.0 };
+	string nume2[] = { "manichiura", "masaj", "jacuzzi"};
+
+	h1.setServicii(3, preturi2, nume2);
+
+
 
 	cout << "------------------Obiectul h1 dupa de modificare" << endl;
 
@@ -421,10 +590,105 @@ void main() {
 	cout << "Numarul de servicii disponibile :" << h1.getnumarServiciiDisponibile() << endl;
 	cout << "Preturi servicii :" << endl;
 	for (int i = 0; i < h1.getnumarServiciiDisponibile(); i++) {
-		cout << "Pret<" << h1.getpreturiServicii()[i] << endl;
-		cout << "Denumire Servicii" << h1.getNumeServicii()[i] << endl;
+		cout << "Pret: " << h1.getpreturiServicii()[i] << endl;
+		cout << "Denumire Servicii: " << h1.getNumeServicii()[i] << endl;
 	}
 	cout << endl << endl;
 
+	cout << ".............Apel Constructor Copiere (obj h10 copiat pe baza obj h7) ......................" << endl << endl;
+	Hotel h10(h7);
+
+	cout << ".................Obiectul h7............................" << endl<< endl;
+
+	cout << "Denumirea hotelului este: " << h7.getDenumireHotel() << endl;
+	cout << "Adresa hotelului este:" << h7.getAdresaHotel() << endl;
+	cout << "Numarul de anjajati este:" << h7.getNumarAngajati() << endl;
+	cout << "Numar camere :" << h7.getNumarCamere() << endl;
+	cout << "Are mic dejun inclus? (0 pentru Nu , 1 pentru DA)  " << h7.getAreMicDejunInclus() << endl;
+	cout << "Pretul pe camera/zi este: " << h7.getPretCameraPeZi() << endl;
+	cout << "Anul deschiderii hotelului eate:" << h7.getAnDeschidereHotel() << endl;
+	cout << "Nume manager Hotel este: " << h7.getNumeManagerHotel() << endl;
+	cout << "Numarul de servicii disponibile :" << h7.getnumarServiciiDisponibile() << endl;
+	cout << "Preturi servicii :" << endl;
+	for (int i = 0; i < h7.getnumarServiciiDisponibile(); i++) {
+		cout << "Pret<" << h7.getpreturiServicii()[i] << endl;
+		cout << "Denumire Servicii" << h7.getNumeServicii()[i] << endl;
+	}
+	cout << endl << endl;
+
+	cout << ".................Obiectul h10............................" << endl << endl;
+
+	cout << "Denumirea hotelului este: " << h10.getDenumireHotel() << endl;
+	cout << "Adresa hotelului este: " << h10.getAdresaHotel() << endl;
+	cout << "Numarul de anjajati este: " << h10.getNumarAngajati() << endl;
+	cout << "Numar camere :" << h10.getNumarCamere() << endl;
+	cout << "Are mic dejun inclus? (0 pentru Nu , 1 pentru DA)  " << h10.getAreMicDejunInclus() << endl;
+	cout << "Pretul pe camera/zi este: " << h10.getPretCameraPeZi() << endl;
+	cout << "Anul deschiderii hotelului eate:" << h10.getAnDeschidereHotel() << endl;
+	cout << "Nume manager Hotel este: " << h10.getNumeManagerHotel() << endl;
+	cout << "Numarul de servicii disponibile :" << h10.getnumarServiciiDisponibile() << endl;
+	cout << "Preturi servicii :" << endl;
+	for (int i = 0; i < h10.getnumarServiciiDisponibile(); i++) {
+		cout << "Pret: " << h10.getpreturiServicii()[i] << endl;
+		cout << "Denumire Servicii: " << h10.getNumeServicii()[i] << endl;
+	}
+	cout << endl << endl;
+
+	h4 = h1;
+
+	cout << ".............Apel OPERATOR = (obj h4 copiat h1) ......................" << endl << endl;
+
+	cout << "...................Obiectul h4 inainte de a fi modificat..............." << endl << endl;
+	cout << "Denumirea hotelului este: " << h4.getDenumireHotel() << endl;
+	cout << "Adresa hotelului este:" << h4.getAdresaHotel() << endl;
+	cout << "Numarul de anjajati este:" << h4.getNumarAngajati() << endl;
+	cout << "Numar camere :" << h4.getNumarCamere() << endl;
+	cout << "Are mic dejun inclus? (0 pentru Nu , 1 pentru DA)  " << h4.getAreMicDejunInclus() << endl;
+	cout << "Pretul pe camera/zi este: " << h4.getPretCameraPeZi() << endl;
+	cout << "Anul deschiderii hotelului eate:" << h4.getAnDeschidereHotel() << endl;
+	cout << "Nume manager Hotel este: " << h4.getNumeManagerHotel() << endl;
+	cout << "Numarul de servicii disponibile :" << h4.getnumarServiciiDisponibile() << endl;
+	cout << "Preturi servicii :" << endl;
+	for (int i = 0; i < h4.getnumarServiciiDisponibile(); i++) {
+		cout << "Pret<" << h4.getpreturiServicii()[i] << endl;
+		cout << "Denumire Servicii" << h4.getNumeServicii()[i] << endl;
+	}
+	cout << endl << endl;
+
+	
+
+	cout << "...................Obiectul macheta h1........................" << endl << endl;
+	cout << "Denumirea hotelului este: " << h1.getDenumireHotel() << endl;
+	cout << "Adresa hotelului este: " << h1.getAdresaHotel() << endl;
+	cout << "Numarul de anjajati este: " << h1.getNumarAngajati() << endl;
+	cout << "Numar camere :" << h1.getNumarCamere() << endl;
+	cout << "Are mic dejun inclus? (0 pentru Nu , 1 pentru DA)  " << h1.getAreMicDejunInclus() << endl;
+	cout << "Pretul pe camera/zi este: " << h1.getPretCameraPeZi() << endl;
+	cout << "Anul deschiderii hotelului eate:" << h1.getAnDeschidereHotel() << endl;
+	cout << "Nume manager Hotel este: " << h1.getNumeManagerHotel() << endl;
+	cout << "Numarul de servicii disponibile :" << h1.getnumarServiciiDisponibile() << endl;
+	cout << "Preturi servicii :" << endl;
+	for (int i = 0; i < h1.getnumarServiciiDisponibile(); i++) {
+		cout << "Pret: " << h1.getpreturiServicii()[i] << endl;
+		cout << "Denumire Servicii: " << h1.getNumeServicii()[i] << endl;
+	}
+	cout << endl << endl;
+
+	cout << "...................Obiectul h4 dupa modificare..............." << endl << endl;
+	cout << "Denumirea hotelului este: " << h4.getDenumireHotel() << endl;
+	cout << "Adresa hotelului este:" << h4.getAdresaHotel() << endl;
+	cout << "Numarul de anjajati este:" << h4.getNumarAngajati() << endl;
+	cout << "Numar camere :" << h4.getNumarCamere() << endl;
+	cout << "Are mic dejun inclus? (0 pentru Nu , 1 pentru DA)  " << h4.getAreMicDejunInclus() << endl;
+	cout << "Pretul pe camera/zi este: " << h4.getPretCameraPeZi() << endl;
+	cout << "Anul deschiderii hotelului eate:" << h4.getAnDeschidereHotel() << endl;
+	cout << "Nume manager Hotel este: " << h4.getNumeManagerHotel() << endl;
+	cout << "Numarul de servicii disponibile :" << h4.getnumarServiciiDisponibile() << endl;
+	cout << "Preturi servicii :" << endl;
+	for (int i = 0; i < h4.getnumarServiciiDisponibile(); i++) {
+		cout << "Pret<" << h4.getpreturiServicii()[i] << endl;
+		cout << "Denumire Servicii" << h4.getNumeServicii()[i] << endl;
+	}
+	cout << endl << endl;
 
 }
