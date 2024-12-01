f5c43f3211c92399cfd71118f1f61e7ff350d72e
sovarizabela22@stud.ase.ro <sovarizabela22@stud.ase.ro>
2024-12-01 19:12:22 +0200

    commit;

--- p3/P3.cpp
+++ p3/P3.cpp
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<string>
+#include<fstream>
+
 using namespace std;
 
 
@@ -55,7 +57,7 @@ public:
 		this->pretIngrediente = NULL;
 
 	}
-	//getteri
+	
 	string getDenumirePreparat()
 	{
 		return this->denumirePreparat;
@@ -102,7 +104,6 @@ public:
 		return this->pretIngrediente;
 	}
 
-	//seteri
 	void setDenumirePreparat(string DenumireNouaPreparat)
 	{
 		this->denumirePreparat = DenumireNouaPreparat;
@@ -200,7 +201,7 @@ public:
 
 	}
 
-	//constructorul de copiere
+
 	Preparat(Preparat&obiectExistent) :codPreparat(codPreparat)
 	{
 		this->denumirePreparat = obiectExistent.denumirePreparat;
@@ -220,7 +221,7 @@ public:
 			this->pretIngrediente[i] = obiectExistent.pretIngrediente[i];
 		}
 	}
-	 //operatorul =
+	
 
 	Preparat& operator=(Preparat& obiectMatrice)
 	{
@@ -262,6 +263,140 @@ public:
 	}
 
 
+	float pretMinim()
+	{
+		
+		float rezultat;
+		rezultat = this->pretIngrediente[0];
+		for (int i = 1; i < this->numarIngrediente; i++)
+		{
+			if (this->pretIngrediente[i]<rezultat)
+			{
+				rezultat = this->pretIngrediente[i];
+			}
+		}
+
+		return rezultat;
+
+	}
+
+	float SumaPret()
+	{
+		float suma = 0;
+		for (int i = 0; i < this->numarIngrediente; i++)
+		{
+			suma = suma + this->pretIngrediente[i];
+		}
+		return suma;
+	}
+
+	float mediaPret()
+	{
+		float media;
+		media = SumaPret() / numarIngrediente;
+		return media;
+	}
+
+
+	void discount(float discount)
+	{
+		if (discount < 0.0f || discount>1.0f)
+		{
+			throw exception("discountul nu este corect");
+		}
+		pretPreparat = pretPreparat - pretPreparat * discount;
+	}
+	
+	//METODA DE ADAUGARE ELEMENT IN VECTOR
+	//METODA DE ELIMINARE DIN VECTOR
+	//Operatorul <=
+	//Operatorul ==
+
+	friend ostream& operator<<(ostream& out, const Preparat& h)
+	{
+		out << "Denumirea Preparatului este: " << h.denumirePreparat << endl;
+		out << "Tipul de mancare este : " << h.tipMancare << endl;
+		out << "Pretul preparatului este : " << h.pretPreparat << endl;
+		out << "Contine Alergeni ? " << (h.contineAlergeni ? "DA" : "NU") << endl;
+		out << "Cod preparat: " << h.codPreparat << endl;
+		out << "Numele restaurantului este : " << h.numeRestaurant << endl;
+		out << "Numarul de ingrediente este : " << h.numarIngrediente << endl;
+		out << "Lista de ingrediente si pretul acestora: " << endl;
+		for (int i = 0; i < h.numarIngrediente; i++)
+		{
+			out << "Ingredient : " << h.listaIngrediente[i] << endl;
+			out << "Pret Ingredient: " << h.pretIngrediente[i] << endl;
+		}
+		out << endl << endl;
+
+		return out;
+	}
+
+
+	
+	friend istream& operator>>(istream& in, Preparat& p)
+	{
+		if (p.numeRestaurant != NULL)
+		{
+			delete[]p.numeRestaurant;
+		}
+		if (p.listaIngrediente != NULL)
+		{
+			delete[]p.listaIngrediente;
+		}
+
+		if (p.pretIngrediente != NULL)
+		{
+			delete[]p.pretIngrediente;
+		}
+		cout << "Introduceti denumirea Preparatului : ";
+		in >> p.denumirePreparat;
+		cout << "Introduceti tipul de mancare: ";
+		in >> p.tipMancare;
+		cout << "Introduceti Pretul preparatului : ";
+		in >> p.pretPreparat;
+		cout << "Contine Alergeni ? ";
+		in >> p.contineAlergeni;
+		cout << "Introduceti numele restaurantului : ";
+		char aux[100];
+		in >> aux;
+		p.numeRestaurant = new char[strlen(aux) + 1];
+		strcpy(p.numeRestaurant, aux);
+		
+		cout << "Introduceti numarul de ingrediente : ";
+		in >> p.numarIngrediente;
+		cout << "Introduceti lista de ingrediente si pretul acestora: ";
+		cout << "Introduceti ingredientele : ";
+		p.listaIngrediente = new string[p.numarIngrediente];
+		p.pretIngrediente = new float[p.numarIngrediente];
+		for (int i = 0; i < p.numarIngrediente; i++)
+		{
+			
+			in >> p.listaIngrediente[i];
+			
+		}
+		cout << "Introduceti preturile";
+		for (int i = 0; i < p.numarIngrediente; i++)
+		{
+			
+			
+			in >> p.pretIngrediente[i];
+		}
+		cout << endl << endl;
+
+		return in;
+
+
+	}
+
+
+	//Opertaorul [] - de indexare
+	//Operatorul () - functie
+	//Operatorul ! - de negatie
+	//Operatorul de autoasignare +=
+	//Operatorul -=
+	//Operatorul de cast
+	//FISERE TEXT SI FISIERE BINARE
 };
 
 string Preparat::OriginePreparat = "Romania";
@@ -321,4 +456,25 @@ void main()
 	m3 = m1;
 	cout << "......Valoare m3 dupa modificare..............." << endl << endl;
 	m3.afisare();
+
+	cout << "m1" <<endl<< m1 << endl << endl;
+
+	/*cin >> m4;*/
+
+	cout << "m4 dupa ce a fost introdus de la tastatura" << endl << m4 << endl << endl;
+	cout << m1 << endl << endl;
+	cout << "Pretul minim al obiectului M1 este:" << m1.pretMinim() << endl;
+	cout << "Suma preturilor obiectului M1 este:" << m1.SumaPret() << endl;
+	cout << "Media preturilor obiectului M1 este:" << m1.mediaPret() << endl;
+
+	cout << m1.getPretPreparat() << endl;
+	m1.discount(0.2);
+	cout << m1.getPretPreparat() << endl;
+	try{
+	
+			m1.discount(1.5f);
+	}catch (exception e)
+	{
+		cout << "eroare" << endl;
+	}
 }
